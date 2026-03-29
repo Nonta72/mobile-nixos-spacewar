336b7c85117c201a5999fe01e5601adfb0aaa98d
Kurt Morris <kurt.morris@me.com>
2026-03-28 20:55:39 -0700

    devices: families: sm7325-mainline: kernel: Update Kernel to 6.19.0 with preliminary WCD9385 patch

--- devices/families/sm7325-mainline/kernel/default.nix
+++ devices/families/sm7325-mainline/kernel/default.nix
@@ -6,14 +6,14 @@
 
 # SC7280 is a compute variant of the SM7325
 mobile-nixos.kernel-builder {
-  version = "6.16.0";
+  version = "6.19.0";
   configfile = ./config.aarch64;
 
   src = fetchFromGitHub {
     owner = "sc7280-mainline";
     repo = "linux";
-    rev = "6cfded7852edc676a16fd61eae38ae030ae9b8f6";
-    hash = "sha256-FRMPG/DgvgVhe9CT9GUqC2MnOTSZQbq6FzwWjpjaFs8=";
+    rev = "2f12f5e50225dcd556455e7a4dc4751832b7be88";
+    hash = "sha256-BNB5qA01bQVh2nCSqLSllw1ktyIiHPV1Ybee+jVvO+0=";
   };
 
   patches = [
